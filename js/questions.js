e4a25c204f7dec9d5f3540164648a3a8d684ce54
Trudy Jones <tjones374@gmail.com>
2020-01-06 11:49:26 -0700

    Moved questions.js to new repo

---
+++ js/questions.js
@@ -0,0 +1,32 @@
+var questions = [
+	{
+		title: 'Commonly used data types DO NOT include',
+		choices: ['strings', 'booleans', 'alerts', 'numbers'],
+		answer: 'alerts'
+	},
+	{
+		title: 'The symbol used for single line comments in JavaScript is',
+		choices: [
+			'double forward slash',
+			'backslash',
+			'double backslash',
+			'quotations'
+		],
+		answer: 'double forward slash'
+	},
+	{
+		title: 'Breaking within a string statement can be done by',
+		choices: ['backslash', 'forward slash', 'period', 'question mark'],
+		answer: 'backslash'
+	},
+	{
+		title: 'What is NOT a type of pop up box available in JavaScript?',
+		choices: ['Alert', 'Confirm', 'Prompt', 'Ask'],
+		answer: 'Ask'
+	},
+	{
+		title: 'A value that can be assigned to a variable or an object is',
+		choices: ['null', 'undefined', 'undeclared', 'defined'],
+		answer: 'null'
+	}
+];
